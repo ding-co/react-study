824ee139a2ca6b84dd9cf684640b56f9ac280d04
ding-co <pangoons@naver.com>
2022-02-02 22:03:06 +0900

    [ADD] React ch 12 - React & Server

---
+++ code/ch12/ex12-4/pets/src/pages/page-3.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <SEO title="Page three" />
+    <h1>Hi from third page</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ThirdPage
